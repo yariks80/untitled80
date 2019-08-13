378d9c1cdb43fc36a48dc5db9699416294d1e5f6
iaroslav.sviatkin <$viatk1N2013>
2019-08-13 13:03:43 +0300

    commit4

--- src/org/no/Main.java
+++ src/org/no/Main.java
@@ -9,5 +9,8 @@ public class Main {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
     }
 }
